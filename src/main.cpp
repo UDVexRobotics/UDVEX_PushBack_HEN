a52f1af626b3953061d20634f53512f4b0ade7aa
MEarley <113404793+MEarley@users.noreply.github.com>
2025-09-20 12:32:28 -0400

    - Added Configuration file for initial drive train - Added arcade drive to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "config.hpp"
+
+extern lemlib::Chassis chassis;
 
 /**
  * A callback function for LLEMU's center button.
@@ -28,11 +32,15 @@ void on_center_button()
  */
 void initialize()
 {
+	// Initialize the LCD
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 	pros::lcd::set_text(2, "UDVEX FTW!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
+
+	// Calibrate the Sensors
+	chassis.calibrate();
 }
 
 /**
@@ -82,20 +90,20 @@ void autonomous() {}
 void opcontrol()
 {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});	// Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6}); // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
 	while (true)
 	{
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 						 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 						 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0); // Prints status of the emulated screen LCDs
 
+		int32_t leftY = master.get_analog(ANALOG_LEFT_Y);  // Gets the Y axis of the left joystick
+		int32_t rightX = master.get_analog(ANALOG_LEFT_X); // Gets the X axis of the right joystick
 		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);	  // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X); // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);					  // Sets left motor voltage
-		right_mg.move(dir + turn);					  // Sets right motor voltage
+		chassis.arcade(leftY, rightX);
+		// int dir = master.get_analog(ANALOG_LEFT_Y);	  // Gets amount forward/backward from left joystick
+		// int turn = master.get_analog(ANALOG_RIGHT_X); // Gets the turn left/right from right joystick
+		// left_mg.move(dir - turn);					  // Sets left motor voltage
+		// right_mg.move(dir + turn);					  // Sets right motor voltage
 		pros::delay(20);							  // Run for 20 ms then update
 	}
 }
