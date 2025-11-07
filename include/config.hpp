d6c404924f4aabd91ef29f80677a63f516530556
Michael <113404793+MEarley@users.noreply.github.com>
2025-11-07 18:10:34 -0500

    PR#2 Library and Initial Driver Control (#2)
    * - Installed LemLibs
    
    * - Added Configuration file for initial drive train
    - Added arcade drive to main
    
    * - Added control for intake and drive train
    
    * - Added Sudoku Logo (Bit Array File is too large to store)

---
+++ include/config.hpp
@@ -0,0 +1,71 @@
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "api.h"
+
+#ifndef _UDVEX_CONFIG_H_
+#define _UDVEX_CONFIG_H_
+
+// Define motors and motor groups here
+pros::MotorGroup right_motors({19,17}, pros::MotorGearset::blue);
+pros::MotorGroup left_motors({-18,-16}, pros::MotorGearset::blue);
+pros::MotorGroup intake_motors({1, 2, -10, 11, -13}, pros::MotorGearset::blue);
+
+// Define sensors
+lemlib::OdomSensors sensors(
+    nullptr, // Vertical tracking wheel 1
+    nullptr, // Vertical tracking wheel 2
+    nullptr, // Horizontal tracking wheel 1
+    nullptr, // Horizontal tracking wheel 2
+    nullptr  // Inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10,  // proportional gain (kP)
+                                              0,   // integral gain (kI)
+                                              7,   // derivative gain (kD)
+                                              0,   // anti windup
+                                              0,   // small error range, in inches
+                                              0, // small error range timeout, in milliseconds
+                                              0,   // large error range, in inches
+                                              0, // large error range timeout, in milliseconds
+                                              0   // maximum acceleration (slew)
+);
+// lemlib::ControllerSettings lateral_controller(10,  // proportional gain (kP)
+//                                               0,   // integral gain (kI)
+//                                               7,   // derivative gain (kD)
+//                                               3,   // anti windup
+//                                               1,   // small error range, in inches
+//                                               100, // small error range timeout, in milliseconds
+//                                               3,   // large error range, in inches
+//                                               500, // large error range timeout, in milliseconds
+//                                               20   // maximum acceleration (slew)
+// );
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2,   // proportional gain (kP)
+                                              0,   // integral gain (kI)
+                                              10,  // derivative gain (kD)
+                                              3,   // anti windup
+                                              1,   // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3,   // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0    // maximum acceleration (slew)
+);
+
+// Create the drivetrain object
+lemlib::Drivetrain drivetrain(&left_motors,             // left motor group
+                              &right_motors,            // right motor group
+                              9,                        // track width (inches)
+                              lemlib::Omniwheel::NEW_4, // wheel diameter (inches)
+                              360,                      // drivetrain RPM
+                              2                         // horizontal drift
+);
+
+// Create the chassis object
+lemlib::Chassis chassis(drivetrain,         // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors             // odometry sensors
+);
+
+#endif // _UDVEX_CONFIG_H_
